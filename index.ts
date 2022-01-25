b6748d9b555e0fb116d542a5207c63870bfa712e
Hiroki SAKABE <hiroki.sakabe@icloud.com>
2022-01-25 12:28:55 +0000

    Refactor for multiple events

--- index.ts
+++ index.ts
@@ -3,70 +3,93 @@ import * as github from "@actions/github";
 import axios from "axios";
 import { promises as fs } from "fs";
 
+type PullRequestPayload = NonNullable<
+  typeof github.context.payload.pull_request
+>;
+
+const notify_review_request = async (
+  payload_pull_request: PullRequestPayload,
+  webhook_url: string
+) => {
+  const reviewers = payload_pull_request.requested_reviewers;
+  const title = payload_pull_request.title;
+  const html_url = payload_pull_request.html_url;
+
+  if (!html_url) {
+    throw new Error("Could not retrieve PR URL.");
+  }
+
+  if (!Array.isArray(reviewers) || reviewers.length === 0) {
+    console.log("Could not retrieve 'reviewers'.");
+    return;
+  }
+
+  const github_slack_id_map = await load_slack_id_map();
+
+  let text = "";
+
+  reviewers.forEach((reviewer) => {
+    const github_username = reviewer.login;
+
+    if (typeof github_username !== "string") {
+      return;
+    }
+
+    let slack_id = github_slack_id_map[github_username];
+
+    if (typeof slack_id !== "string") {
+      // If Slack member ID is not specified, use GitHub username instead.
+      slack_id = github_username;
+    }
+
+    text = text.concat(`<@${slack_id}> `);
+  });
+
+  if (typeof title === "string") {
+    text = text.concat(`\n*Review requested: * <${html_url}|${title}>`);
+  } else {
+    text = text.concat(`\n*Review requested: * ${html_url}`);
+  }
+
+  await axios.post(webhook_url, { text });
+};
+
+const load_slack_id_map = async () => {
+  let github_slack_id_map: { [github_username: string]: unknown } = {};
+  try {
+    github_slack_id_map = github_slack_id_map = JSON.parse(
+      await fs.readFile(".github/slack-id.json", "utf8")
+    );
+  } catch (e) {
+    // TODO: Add error handling other than ENOENT.
+  }
+  return github_slack_id_map;
+};
+
 const main = async () => {
   try {
     const action = github.context.payload.action;
-
     const payload_pull_request = github.context.payload.pull_request;
 
-    const reviewers = payload_pull_request?.requested_reviewers;
-    const title = payload_pull_request?.title;
-    const html_url = payload_pull_request?.html_url;
+    if (!payload_pull_request) {
+      console.log("Only supports 'pull_request' event.");
+      return;
+    }
 
     const url = process.env.PR_MESSAGE_SLACK_WEBHOOK_URL; // https://hooks.slack.com/...
     if (!url) {
       throw new Error("PR_MESSAGE_SLACK_WEBHOOK_URL is not set.");
     }
 
-    if (!html_url) {
-      throw new Error("Could not retrieve PR URL.");
-    }
-
-    if (action !== "review_requested") {
-      console.log("'types' only supports 'review_requested'.");
-      return;
-    }
-
-    if (!Array.isArray(reviewers) || reviewers.length === 0) {
-      console.log("Could not retrieve 'reviewers'.");
-      return;
-    }
-
-    let github_slack_id_map: { [github_username: string]: unknown } = {};
-    try {
-      github_slack_id_map = JSON.parse(
-        await fs.readFile(".github/slack-id.json", "utf8")
-      );
-    } catch (e) {
-      // TODO: Add error handling other than ENOENT.
-    }
-
-    let text = "";
-
-    reviewers.forEach((reviewer) => {
-      const github_username = reviewer.login;
-
-      if (typeof github_username !== "string") {
-        return;
+    switch (action) {
+      case "review_requested": {
+        await notify_review_request(payload_pull_request, url);
+        break;
       }
-
-      let slack_id = github_slack_id_map[github_username];
-
-      if (typeof slack_id !== "string") {
-        // If Slack member ID is not specified, use GitHub username instead.
-        slack_id = github_username;
+      default: {
+        console.log("'types' only supports 'review_requested'.");
       }
-
-      text = text.concat(`<@${slack_id}> `);
-    });
-
-    if (typeof title === "string") {
-      text = text.concat(`\n*Review requested: * <${html_url}|${title}>`);
-    } else {
-      text = text.concat(`\n*Review requested: * ${html_url}`);
     }
-
-    axios.post(url, { text });
   } catch (e) {
     core.setFailed(e instanceof Error ? e.message : JSON.stringify(e));
   }
